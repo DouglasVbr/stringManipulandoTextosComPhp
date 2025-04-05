ca525984b50d91c778929b4b9aef54d8f95c3165
Douglas <douglascanal1998@gmail.com>
2025-04-05 12:11:53 -0300

    strings manipulando textos com php

--- usuario.php
+++ usuario.php
@@ -3,9 +3,16 @@
 $email = 'douglascanal1998@gmail.com';
 $senha = 'Iaomiofaile06!';
 
-$posicaoDoArroba = strpos($email, '@', '16');
+echo mb_strlen($senha) . PHP_EOL;
+if (mb_strlen($senha) < 8) {
+    echo 'Senha insegura' . PHP_EOL;
+}
 
-echo substr($email, '0', $posicaoDoArroba) . PHP_EOL;
+$posicaoDoArroba = strpos($email, '@');
+
+$usuario = substr($email, 0, $posicaoDoArroba);
+
+echo mb_strtoupper($usuario) . PHP_EOL;
 
 echo substr($email, $posicaoDoArroba + 1) . PHP_EOL;
 
